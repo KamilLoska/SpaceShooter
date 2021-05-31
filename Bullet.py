69e9e78d93e3ebc35aa2c87bf81302b16900c908
KamilLoska <72936376+KamilLoska@users.noreply.github.com>
2021-05-31 09:35:27 +0200

    Update Bullet.py

--- Bullet.py
+++ Bullet.py
@@ -1,9 +1,9 @@
 import pygame
 from pygame.sprite import Sprite
-import time
+
 
 class Pocisk(Sprite):
-    def __init__(self,ai_settings, screen, player):
+    def __init__(self, ai_settings, screen, player):
         super(Pocisk, self).__init__()
         self.screen = screen
 
@@ -12,18 +12,15 @@ class Pocisk(Sprite):
         self.rect.center = player.rect.center
         self.rect.right = player.rect.right
 
-
         # Położenie pocisku jest zdefiniowane za pomocą wartości zmiennoprzecinkowej
         self.x = float(self.rect.x)
         self.x = float(self.rect2.x)
 
-
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
 
-
     def update(self):
-        #Poruszanie pociskiem po ekranie
+        # Poruszanie pociskiem po ekranie
         self.x = self.speed_factor
         self.rect.x += self.x
 
