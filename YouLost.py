44c1b4943297638eea335f4bf9cb8db9b4bca7e4
KamilLoska <72936376+KamilLoska@users.noreply.github.com>
2021-06-15 07:49:19 +0200

    Update YouLost.py

--- YouLost.py
+++ YouLost.py
@@ -1,20 +1,21 @@
 import pygame
-import random
-from Player import Character
-import time
 import sys
-from pygame.locals import *
+import os
 
 
-
-class YouLost():
+class YouLost(object):
 
     def __init__(self, ai_settings, screen):
 
         self.screen = screen
         self.ai_settings = ai_settings
-        self.image = pygame.image.load('C:/Users/Kamil/Pictures/youlost2.png').convert_alpha()
+        self.image = pygame.image.load(resource_path('youlost2.png')).convert_alpha()
         self.rect = self.image.get_rect()
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
